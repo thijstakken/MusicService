bd029bd87988272d9e5b4a155ebedcbb7e14585e
Thijs Takken <23289714+Thijsie88@users.noreply.github.com>
2022-07-07 11:03:31 +0200

    :art: Reformated the downloaded and playlists files without the .txt file extension. Makes more sense in Linux 🐧

--- main.py
+++ main.py
@@ -41,13 +41,13 @@ ydl_opts = {
     'outtmpl': './music/%(playlist)s/%(title)s-%(id)s.%(ext)s', # save music to the /music folder. and it's corrosponding folder which will be named after the playlist name
     'simulate': False,                                          # to dry test the YT-DL, if set to True, it will skip the downloading. Can be True/False
     'cachedir': False,                                          # turn off caching, this should mitigate 403 errors which are commonly seen when downloading from Youtube
-    'download_archive': './database/downloaded.txt',            # this will update the .txt file which serves as a database for which files have already been downloaded
+    'download_archive': './database/downloaded',            # this will update the downloads file which serves as a database/archive for which songs have already been downloaded, so it don't downloads them again
     'nocheckcertificate': True,                                 # mitigates YT-DL bug where it wrongly examins the server certificate, so therefore, ignore invalid certificates for now, to mitigate this bug
 }
 
 # reads and saves playlist URL's in a list
 def getPlaylistURLs():
-    with open('./database/playlists.txt') as file:
+    with open('./database/playlists') as file:
         lines = [line.rstrip() for line in file]
     return(lines)
 
