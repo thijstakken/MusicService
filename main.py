423d0e34ac9bc5b9bf36f39144076a0fb699780b
Thijs Takken <23289714+Thijsie88@users.noreply.github.com>
2022-10-17 21:36:05 +0200

    :bug: fixed #27 added fallback format for native mp3s to enable support

--- main.py
+++ main.py
@@ -28,21 +28,21 @@ def my_hook(d):
 # Configure YouTube DL options
 ydl_opts = {
     'writethumbnail': True,
-    'format': 'bestaudio[asr<=44100]/best[asr<=44100]',         # using asr 44100 as max, this mitigates exotic compatibility issues with certain mediaplayers
+    'format': 'bestaudio[asr<=44100]/best[asr<=44100]/bestaudio',   # using asr 44100 as max, this mitigates exotic compatibility issues with certain mediaplayers, and allow bestaudio as a fallback for direct mp3s
     'postprocessors': [{    
-        'key': 'FFmpegExtractAudio',                            # use FFMPEG and only save audio
-        'preferredcodec': 'mp3',                                # convert to MP3 format
-        #'preferredquality': '192',                             # with not specifying a preffered quality, the original bitrate will be used, therefore skipping one unnecessary conversion and keeping more quality
+        'key': 'FFmpegExtractAudio',                                # use FFMPEG and only save audio
+        'preferredcodec': 'mp3',                                    # convert to MP3 format
+        #'preferredquality': '192',                                 # with not specifying a preffered quality, the original bitrate will be used, therefore skipping one unnecessary conversion and keeping more quality
         },
-    {'key': 'EmbedThumbnail',},                                 # embed the Youtube thumbnail with the MP3 as coverart.
+    {'key': 'EmbedThumbnail',},                                     # embed the Youtube thumbnail with the MP3 as coverart.
     ],
     'logger': MyLogger(),
     'progress_hooks': [my_hook],
-    'outtmpl': './music/%(playlist)s/%(title)s-%(id)s.%(ext)s', # save music to the /music folder. and it's corrosponding folder which will be named after the playlist name
-    'simulate': False,                                          # to dry test the YT-DL, if set to True, it will skip the downloading. Can be True/False
-    'cachedir': False,                                          # turn off caching, this should mitigate 403 errors which are commonly seen when downloading from Youtube
-    'download_archive': './config/downloaded',                  # this will update the downloads file which serves as a database/archive for which songs have already been downloaded, so it don't downloads them again
-    'nocheckcertificate': True,                                 # mitigates YT-DL bug where it wrongly examins the server certificate, so therefore, ignore invalid certificates for now, to mitigate this bug
+    'outtmpl': './music/%(playlist)s/%(title)s-%(id)s.%(ext)s',     # save music to the /music folder. and it's corrosponding folder which will be named after the playlist name
+    'simulate': False,                                              # to dry test the YT-DL, if set to True, it will skip the downloading. Can be True/False
+    'cachedir': False,                                              # turn off caching, this should mitigate 403 errors which are commonly seen when downloading from Youtube
+    'download_archive': './config/downloaded',                      # this will update the downloads file which serves as a database/archive for which songs have already been downloaded, so it don't downloads them again
+    'nocheckcertificate': True,                                     # mitigates YT-DL bug where it wrongly examins the server certificate, so therefore, ignore invalid certificates for now, to mitigate this bug
 }
 
 # reads and saves playlist URL's in a list
