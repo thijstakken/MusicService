37c78c46a4bc679e199b207509e25aa1bead6565
Thijs Takken <23289714+thijstakken@users.noreply.github.com>
2024-02-27 20:15:46 +0100

    Clean up init

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -19,5 +19,87 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
+
+
+    # # start running the run_schedule function in the background
+    # # get all the playlists/songs
+    # music_list = Music.query.all()
+    # settings = WebDAV.query.filter_by(id=1).first()
+    # # iterate over the playlists/songs
+    # for music in music_list:
+    #     # make sure the playlist/song is set to be monitored
+    #     if music.monitored is True and settings is not None:
+    # # get the interval value for each playlist/song
+    #         scheduleJobs(music, settings)
+    # print('here are all jobs', schedule.get_jobs())
+
+    # #interval_check()
+
+    # # start the schedule in the background as a seperated thread from the main thread
+    # # this is needed to let the scheduler run in the background, while the main thread is used for the webserver
+    # t = threading.Thread(target=run_schedule, args=(app.app_context(),), daemon=True)
+    # t.start()
+
+
+    # setting general variables
+    # 'music' always use music as local, this can't be changed at the moment, due to some hardcoding
+    # make the localDirectory global so it can be used in other functions
+    #global localDirectory
+"""
+The line of code you've shared is in Python and it's making use of the `global` keyword.
+The `global` keyword in Python is used to indicate that a variable is a global variable, meaning it can be accessed from anywhere in the code, not just in the scope where it was declared. 
+In this case, `localDirectory` is being declared as a global variable. This means that `localDirectory` can be used, modified, or re-assigned in any function within this Python script, not just the function where it's declared.
+However, it's important to note that using global variables can make code harder to understand and debug, because any part of the code can change the value of the variable. It's generally recommended to use local variables where possible, and pass data between functions using arguments and return values.
+"""
+    #localDirectory = 'music'
+
+# # check if file ../download_archive/downloaded exists
+# archive_directory = '../download_archive/'
+# archive_file = 'downloaded'
+# download_archive = os.path.join(archive_directory, archive_file)
+
+# if os.path.isfile(download_archive) == False:
+#     # tries to create the archive directory and file
+
+#     print("Download archive does not exist")
+
+#     # tries to create archive directory
+#     try:
+#         print("Creating directory...")
+#         output = os.mkdir(archive_directory)
+#         print("Directory '% s' created" % archive_directory)
+#     except OSError as error:
+#         print(error)
+
+#     # tries to create archive file
+#     try:
+#         print("Creating file...")
+#         open(download_archive, 'x')
+#         print("File '% s' created" % download_archive)
+#     except OSError as error:
+#         print(error)
+
+#     else:
+#         print("Download archive exists, nothing to do...")
+
+
+
 # gets all the routes for the web application
 from webapp import routes, models
+
+
+# print Python version for informational purposes
+#print("Python version: "+ sys.version)
+
+# welcome message
+print("Starting MusicService")
+version = '2024.3'
+print("Version:", version)
+
+# blueprint for auth routes in our app
+# from .auth import auth as auth_blueprint
+# app.register_blueprint(auth_blueprint)
+
+# blueprint for non-auth parts of app
+#from .routes import main as main_blueprint
+#app.register_blueprint(main_blueprint)
